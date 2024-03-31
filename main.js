9d392cc8decb1aa900eb2a689be27309be05543a
congnghiahieu <21020540@vnu.edu.vn>
2024-03-31 18:34:32 +0700

    feat: update new homework

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
-function getDiscountPercentage(price) {
-  if (typeof price !== 'number') {
-    throw new Error('Invalid input');
+/**
+ * @description
+ * @author hieucien
+ * @date 31/03/2024
+ * @param {number} firstPrice
+ * @param {string} category
+ * @returns {*}
+ */
+function getFinalPrice(firstPrice, category) {
+  if (typeof firstPrice !== 'number' || typeof category !== 'string') {
+    throw new Error('Invalid input type');
   }
 
-  if (price <= 0 || price > 10000) {
-    throw new Error('Invalid input');
+  if (firstPrice <= 0 || firstPrice > 10000) {
+    throw new Error('Invalid firstPrice');
   }
 
-  if (price < 1000) {
-    return 0;
+  let discountPercentage = 0;
+
+  switch (category) {
+    case 'Food & Drink':
+      discountPercentage = 0;
+      break;
+    case 'Clothes':
+      discountPercentage = 5;
+      break;
+    case 'Electronics':
+      discountPercentage = 10;
+      break;
+    default:
+      throw new Error('Invalid category');
   }
 
-  if (price < 2000) {
-    return 5;
-  }
-  if (price < 5000) {
-    return 8;
-  }
-  if (price < 7000) {
-    return 12;
-  }
-  if (price < 8500) {
-    return 15;
+  if (5000 <= firstPrice && firstPrice < 8000) {
+    discountPercentage += 5;
+  } else if (firstPrice >= 8000) {
+    discountPercentage += 10;
   }
 
-  return 18;
+  console.log(discountPercentage);
+
+  return Number.parseFloat((firstPrice * (1 - discountPercentage / 100)).toFixed(2));
 }
 
-module.exports = { getDiscountPercentage };
+console.log(getFinalPrice(9000, 'Food & Drink'));
+console.log(getFinalPrice(6000, 'Clothes'));
+console.log(getFinalPrice(8500, 'Electronics'));
+
+module.exports = { getFinalPrice };
--- main.test.js
+++ main.test.js
@@ -1,44 +1,47 @@
-const { getDiscountPercentage } = require('./main');
+const { getFinalPrice } = require('./main');
 
-test('Kiểm thử hàm getDiscountPercentage bằng bảng quyết định', () => {
-  expect(() => getDiscountPercentage(-1000)).toThrow(Error);
-  expect(getDiscountPercentage(500)).toEqual(0);
-  expect(getDiscountPercentage(1500)).toEqual(5);
-  expect(getDiscountPercentage(3500)).toEqual(8);
-  expect(getDiscountPercentage(6800)).toEqual(12);
-  expect(getDiscountPercentage(7200)).toEqual(15);
-  expect(getDiscountPercentage(9000)).toEqual(18);
-  expect(() => getDiscountPercentage(15000)).toThrow(Error);
+test('Kiểm thử hàm getFinalPrice bằng bảng quyết định', () => {
+  expect(() => getFinalPrice('abc', 123)).toThrow('Invalid input type');
+  expect(() => getFinalPrice(0, 'Food & Drink')).toThrow('Invalid firstPrice');
+
+  expect(() => getFinalPrice(2500, 'Clothes')).toEqual(2250);
+  expect(() => getFinalPrice(6000, 'Electronics')).toEqual(5100);
+
+  expect(() => getFinalPrice(9000, 'abc')).toThrow('Invalid category');
+  expect(() => getFinalPrice(99999, 'abc')).toThrow('Invalid firstPrice');
 });
 
-test('Kiểm thử hàm getDiscountPercentage bằng giá trị biên', () => {
-  expect(() => getDiscountPercentage(-1)).toThrow(Error);
-  expect(() => getDiscountPercentage(0)).toThrow(Error);
-  expect(getDiscountPercentage(1)).toEqual(0);
-  expect(getDiscountPercentage(5000)).toEqual(12);
-  expect(getDiscountPercentage(9999)).toEqual(18);
-  expect(getDiscountPercentage(10000)).toEqual(18);
-  expect(() => getDiscountPercentage(10001)).toThrow(Error);
+test('Kiểm thử hàm getFinalPrice bằng giá trị biên mạnh', () => {
+  expect(() => getFinalPrice(5000, 123)).toThrow('Invalid input type');
+  expect(() => getFinalPrice(5000, 'Food & Drink')).toEqual(4750);
+  expect(() => getFinalPrice(5000, 'abc')).toThrow('Invalid category');
+
+  expect(() => getFinalPrice(0, 'Food & Drink')).toThrow('Invalid firstPrice');
+  expect(() => getFinalPrice(1, 'Clothes')).toEqual(0.95);
+  expect(() => getFinalPrice(2, 'Electronics')).toEqual(1.8);
+  expect(() => getFinalPrice(9999, 'Food & Drink')).toEqual(8999.1);
+  expect(() => getFinalPrice(10000, 'Clothes')).toEqual(8500);
+  expect(() => getFinalPrice(10001, 'Electronics')).toThrow('Invalid firstPrice');
 });
 
-test('Kiểm thử hàm getDiscountPercentage bằng kiểm thử luồng điều khiển với độ đo C2', () => {
-  expect(() => getDiscountPercentage('hello')).toThrow(Error);
-  expect(() => getDiscountPercentage(15000)).toThrow(Error);
-  expect(getDiscountPercentage(500)).toEqual(0);
-  expect(getDiscountPercentage(1500)).toEqual(5);
-  expect(getDiscountPercentage(3500)).toEqual(8);
-  expect(getDiscountPercentage(6800)).toEqual(12);
-  expect(getDiscountPercentage(7200)).toEqual(15);
-  expect(getDiscountPercentage(9000)).toEqual(18);
+test('Kiểm thử hàm getFinalPrice bằng kiểm thử luồng điều khiển với độ đo C2', () => {
+  expect(() => getFinalPrice('abc', 123)).toThrow('Invalid input type');
+  expect(() => getFinalPrice(0, 'abc')).toThrow('Invalid firstPrice');
+  expect(() => getFinalPrice(1, 'abc')).toThrow('Invalid category');
+
+  expect(() => getFinalPrice(1000, 'Food & Drink')).toEqual(1000);
+  expect(() => getFinalPrice(9000, 'Clothes')).toEqual(8075);
+  expect(() => getFinalPrice(7000, 'Electronics')).toEqual(5950);
 });
 
-test('Kiểm thử hàm getDiscountPercentage bằng kiểm thử luồng dữ liệu với độ phủ all-uses', () => {
-  expect(() => getDiscountPercentage('hello')).toThrow(Error);
-  expect(() => getDiscountPercentage(15000)).toThrow(Error);
-  expect(getDiscountPercentage(500)).toEqual(0);
-  expect(getDiscountPercentage(1500)).toEqual(5);
-  expect(getDiscountPercentage(3500)).toEqual(8);
-  expect(getDiscountPercentage(6800)).toEqual(12);
-  expect(getDiscountPercentage(7200)).toEqual(15);
-  expect(getDiscountPercentage(9000)).toEqual(18);
+test('Kiểm thử hàm getFinalPrice bằng kiểm thử luồng dữ liệu với độ phủ all-uses', () => {
+  expect(() => getFinalPrice('abc', 123)).toThrow('Invalid input type');
+  expect(() => getFinalPrice(0, 'abc')).toThrow('Invalid firstPrice');
+  expect(() => getFinalPrice(1, 'abc')).toThrow('Invalid category');
+
+  expect(() => getFinalPrice(1000, 'Food & Drink')).toEqual(1000);
+  expect(() => getFinalPrice(7000, 'Food & Drink')).toEqual(6650);
+  expect(() => getFinalPrice(9000, 'Food & Drink')).toEqual(8100);
+  expect(() => getFinalPrice(6000, 'Clothes')).toEqual(5400);
+  expect(() => getFinalPrice(8500, 'Electronics')).toEqual(6800);
 });
